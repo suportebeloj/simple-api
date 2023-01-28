65a692758fb5f12d295b51a259c81c9e8f566f47
Carlos Eduardo <suportebeloj@gmail.com>
2023-01-28 16:34:23 -0300

    fix nginx

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app /etc/nginx/sites-enabled/
+sudo cp -rf app /etc/nginx/sites-available/app
+chmod 710 $PWD
 
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
