9894d62d32526545187d76c225012a83c97ff9c7
Carlos Eduardo <suportebeloj@gmail.com>
2023-01-29 14:14:50 -0300

    fix nginx

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf bot.plutusfi.io /etc/nginx/sites-available/bot.plutusfi.io
 chmod 710 $PWD
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/bot.plutusfi.io /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
