bd8ce1119d5aed74e517e1376fb34a47db20c99c
Carlos Eduardo <suportebeloj@gmail.com>
2023-01-29 15:43:29 -0300

    fix nginx setup

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf bot.plutusfi.io /etc/nginx/sites-available/bot.plutusfi.io
+sudo cp -rf mm.plutusfi.io /etc/nginx/sites-available/mm.plutusfi.io
 chmod 710 $PWD
 
-sudo ln -s /etc/nginx/sites-available/bot.plutusfi.io /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/mm.plutusfi.io /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
